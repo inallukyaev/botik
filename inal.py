305b7f534d1af7ca18126e23eb0f4ffa327164f9
Inal Lukyaev <inal-lukyaev@mail.ru>
2020-10-14 21:10:07 +0300

    Sec version22

--- inal.py
+++ inal.py
@@ -1,7 +1,8 @@
+import logging, ephem
 from glob import glob
-import logging
 from random import choice
 import settings
+
 
 from telegram.ext import Updater, CommandHandler, MessageHandler , Filters
 
@@ -30,9 +31,29 @@ def send_cat_picture(bot, update):
     cat_pic = choice(cat_list)
     bot.send_photo(chat_id = update.message.chat_id, photo=open(cat_pic,"rb"))
 
+def get_constel(bot, update):
+    planets = {"Mercury": ephem.Mercury("2020/10/13"), 
+    "Venus": ephem.Venus("2020/10/13"), 
+    "Mars" : ephem.Mars("2020/10/13"), 
+    "Jupiter": ephem.Jupiter("2020/10/13"), 
+    "Saturn": ephem.Saturn("2020/10/13"), 
+    "Uranus": ephem.Uranus("2020/10/13"), 
+    "Neptune": ephem.Neptune("2020/10/13")}
+ 
+    planet_input = str(update.message.text.split()[1]).lower().capitalize()  
+    if planet_input in planets:
+        print(ephem.constellation(planets[planet_input]))
+        update.message.reply_text(ephem.constellation(planets[planet_input]))
+        
+ 
+    
+   
+   
 
-
-
+ 
+   
+ 
+         
 def main():
     mybot = Updater(settings.API_KEY,request_kwargs=settings.PROXY)
 
@@ -41,7 +62,10 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", get_constel))
+    
     dp.add_handler(CommandHandler("cat", send_cat_picture))
+    
     
     
     dp.add_handler(MessageHandler(Filters.text,talk_to_me))
